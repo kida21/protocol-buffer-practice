fe57e21dda8b69156936af781e79c97aa8de96f5
kida21 <kidusp22@gmail.com>
2025-04-02 14:57:13 +0300

    Update server.go

--- Grpc server/server.go
+++ Grpc server/server.go
@@ -1,19 +1,31 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
+    "google.golang.org/grpc"
+	pb"grpcserver/proto/gen"
 
-	"google.golang.org/grpc"
+	
 )
+type server struct{
+ pb.UnimplementedCalculateServer
+}
+func (s *server) Add(ctx context.Context,req *pb.AddRequest)(*pb.AddResponse,error){
 
-func main() {
+	return &pb.AddResponse{
+		Result: req.A+req.B,
+	},nil
+}
+func main() { 
 	port:=":50051"
 	listen,err:=net.Listen("tcp",port)
 	if err!=nil{
 		log.Fatal("failed to listen",err)
 	}
 	grpcServer:=grpc.NewServer()
+	pb.RegisterCalculateServer(grpcServer,&server{})
 	log.Println("Server running on..",port)
 	err=grpcServer.Serve(listen)
 	if err!=nil{
